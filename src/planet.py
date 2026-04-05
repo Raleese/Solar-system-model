0050250e949d4c200159072316b5a1aa07a20779
Raleese <titaslr38@gmail.com>
2026-04-06 01:12:24 +0300

    Added zoom functionality

--- src/planet.py
+++ src/planet.py
@@ -11,22 +11,25 @@ class Planet:
 
         self.angle = 0
 
-    def draw(self, screen, sun_world_x, sun_world_y, camera_x=0, camera_y=0):
+    def draw(self, screen, sun_world_x, sun_world_y, zoom, camera_x=0, camera_y=0):
         self.angle += self.orbital_period
         planet_world_x = sun_world_x + math.cos(self.angle) * self.distance_from_sun
         planet_world_y = sun_world_y + math.sin(self.angle) * self.distance_from_sun
 
-        planet_screen_x = planet_world_x - camera_x
-        planet_screen_y = planet_world_y - camera_y
-        
         sun_screen_x = sun_world_x - camera_x
         sun_screen_y = sun_world_y - camera_y
+
+        # Apply zoom to the planet's orbital offset from the sun.
+        offset_x = (planet_world_x - sun_world_x) * zoom
+        offset_y = (planet_world_y - sun_world_y) * zoom
+        planet_screen_x = sun_screen_x + offset_x
+        planet_screen_y = sun_screen_y + offset_y
 
         pygame.draw.circle(
             screen,
             "gray",
             (int(sun_screen_x), int(sun_screen_y)),
-            self.distance_from_sun,
+            int(self.distance_from_sun * zoom),
             1
         )
-        pygame.draw.circle(screen, self.color, (int(planet_screen_x), int(planet_screen_y)), self.radius)
+        pygame.draw.circle(screen, self.color, (int(planet_screen_x), int(planet_screen_y)), int(self.radius * zoom))
